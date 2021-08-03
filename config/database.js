46f425f775ff1bdd09c708b8d04bdf1d5dc3e2c0
omrishooshan <omrishooshan@gmail.com>
2021-08-03 14:21:17 +0300

    changes for heroku

--- config/database.js
+++ config/database.js
@@ -1,11 +1,23 @@
 let mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/nearest_AttractionDB'); //for local host 
+// mongoose.connect('mongodb://localhost:27017/nearest_AttractionDB'); //for local host 
 
 
-let db = mongoose.connection
 
-db.once('open',()=>{console.log("connected")})
+mongoose.connect('mongodb+srv://omrishooshan:dsadsadpjo@cluster0.pzwqb.mongodb.net/nearest_AttractionDB?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(()=>{
+    console.log('database connected')
+}).catch(err=>{
+    console.log("database error:"+err)
+})
+
+// let db = mongoose.connection
+
+// db.once('open',()=>{console.log("connected")})
 
 
 
