1eb7852f14f2f65ee0f5ccb37c979e56aba6fe15
omrishooshan <omrishooshan@gmail.com>
2021-08-03 14:31:36 +0300

    cors

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
 
-  const allowedOrigins = ['http://localhost:3000','https://attractionsomri.herokuapp.com/attractions'];
+  const allowedOrigins = ['http://localhost:3000','https://attractionsomri.herokuapp.com'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
